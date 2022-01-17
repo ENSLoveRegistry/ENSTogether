c4dfa9b4db86ab45478b788093160e94303c1954
Javier Szyfer <jszyfer@gmail.com>
2022-01-17 17:48:51 -0300

    new contract deployed

--- config/contractAddress.js
+++ config/contractAddress.js
@@ -1,3 +1,3 @@
-const contractAddress = "0x18736b8b30316700198cdc5FAdfA2A3A64EC54C4";
-const nftContractAddress = "0xE6BE64b59633D6c1F7289A0B90DDfa9a6E924A85";
+const contractAddress = "0xb2b02D6CfcEF77F13aaC909bd4af54646bFA2a14";
+const nftContractAddress = "0x0B92948D3aEcfa78eA636D108A041DDfc644F1C4";
 (module.exports = contractAddress), nftContractAddress;
