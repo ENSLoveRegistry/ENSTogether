ec196b257428ad9ff98f61a084373dea99d3e3ef
Javier Szyfer <jszyfer@gmail.com>
2022-01-22 11:02:10 -0300

    tet

--- pages/registry.js
+++ pages/registry.js
@@ -12,13 +12,13 @@ export default function Registry() {
     <>
       <div className="flex flex-col px-4 min-h-screen pt-10 sm:pt-24 lg:pt-0  justify-center space-y-4 lg:space-y-0  lg:flex-row ">
         <div className="flex flex-col space-y-4 mt-20 sm:flex-row sm:justify-start sm:mt-20 sm:space-y-0 md:mt-0 md:justify-center md:space-x-10  lg:fixed lg:z-30 top-0 left-0 bottom-0  lg:flex-col lg:items-center lg:justify-center lg:min-h-scren lg:space-x-0 lg:space-y-8 text-rose-700 lg:w-92 lg:pl-24">
-          <div className="shadow shadow-rose-300/40 flex flex-col px-8 py-4 sm:mr-8 sm:p-10 bg-rose-100 sm:bg-rose-100 rounded-3xl items-start md:mr-0">
+          <div className="shadow shadow-rose-300/50 flex flex-col px-8 py-4 sm:mr-8 sm:p-10 bg-rose-100 sm:bg-rose-100 rounded-3xl items-start md:mr-0">
             <p className="text-2xl font-light">Love Proposals:</p>
             <p className="text-rose-600 font-bold text-6xl">
               {proposalsCounter}
             </p>
           </div>
-          <div className=" shadow shadow-rose-300/40  flex flex-col px-8 py-4 sm:p-10 bg-rose-100 sm:bg-rose-100 rounded-3xl items-start ">
+          <div className=" shadow shadow-rose-300/50  flex flex-col px-8 py-4 sm:p-10 bg-rose-100 sm:bg-rose-100 rounded-3xl items-start ">
             <p className="text-2xl font-light">Registry Count:</p>
             <p className="text-rose-600 font-bold text-6xl">
               {registryCounter}
